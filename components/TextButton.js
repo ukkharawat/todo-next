909407edc8aba2bfe8383ad4110b86ef6bced69b
thetong <ukkharawat.uim@gmail.com>
2020-07-28 17:26:00 +0700

    fix: change function to Component Extender

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,17 +1,20 @@
+import { Component } from "react";
+
 const style = {
   borderRadius: '4px',
   padding: '8px',
   border: 'None',
   boxShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
-
 }
 
-const TextButton = ({ text, onClick }) => {
-  return (
-    <button style={style} onClick={onClick}>
-      { text }
-    </button>
-  )
+class TextButton extends Component {
+  render() {
+    return (
+      <button style={style} onClick={this.props.onClick}>
+        { this.props.text }
+      </button>
+    )
+  }
 }
 
 export default TextButton;
